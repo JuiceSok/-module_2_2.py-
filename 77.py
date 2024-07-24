127fe5022f67da26354ccf887f01ddd5bb6189b6
JuiceSok <juice23071992@gmail.com>
2024-07-24 19:42:16 +0600

    Update 77.py

--- 77.py
+++ 77.py
@@ -1,16 +1,9 @@
-while True:
-
-    first = int(input('Введите число:'))
-    second = int(input('Введите число:'))
-    third = int(input('Введите число:'))
-    x = first
-    y = second
-    z = third
-
-    print(x, y, z, sep="\n")
-    if x == y and y == z:
-        print('ответ:', '3')
-    elif x != y and y != z:
-        print('ответ:', '0')
-    elif x == y or y == z or z == x:
-        print('ответ:', '2')
+first = int(input('Введите число:'))
+second = int(input('Введите число:'))
+third = int(input('Введите число:'))
+if first == second == third:
+    print('Вывод: 3')
+elif first == second or first == third or second == third:
+    print('Вывод: 2')
+else:
+    print('Вывод: 0')
